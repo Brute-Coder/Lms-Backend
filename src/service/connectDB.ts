98cbc1b39d1e3442d98a7b5043f53092deb4d684
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-09-01 02:07:36 +0530

    fix: Checking out

--- src/service/connectDB.ts
+++ src/service/connectDB.ts
@@ -4,7 +4,9 @@ import config from "../config/config"
 export default {
   connect: async () => {
     try {
+       
       await mongoose.connect(config.DATABASE_URL as string)
+       
       return mongoose.connection
     } catch (err) {
       throw err
